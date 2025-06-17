8f66971c5589699ce0c6bc5d3a87ea09998549f0
yjein <covee.dev@gmail.com>
2025-06-17 15:41:45 +0900

    [add] password 암호화 코드 추가
    - 암호화를 위해 crypto 적용

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+
 export const formatDate = (rawDate?: Date) => {
   if (!rawDate) return "";
 
@@ -7,4 +9,16 @@ export const formatDate = (rawDate?: Date) => {
   const date = String(dObj.getDate()).padStart(2, "0");
 
   return `${year}.${month}.${date}`;
+};
+
+export const generateSaltedHash = (pw: string): string => {
+  const pwSalt = process.env.PW_SALT;
+  if (pwSalt === undefined) {
+    throw Error("Env Error");
+  }
+  const hashedPassword = crypto
+    .createHmac("sha3-512", pwSalt)
+    .update(pw)
+    .digest("hex");
+  return hashedPassword;
 };
